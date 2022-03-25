54d39f41403cff842652f8bcd3d485c695a4a838
Yusung <qkrdbtjd15@naver.com>
2022-03-25 17:42:01 +0900

    Engagement Entity

---
+++ spring-calendar-project/calendar-core/src/main/java/org/example/calendarproject/core/doamin/entity/Engagement.java
@@ -0,0 +1,30 @@
+package org.example.calendarproject.core.doamin.entity;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.example.calendarproject.core.doamin.Event;
+import org.example.calendarproject.core.doamin.RequestStatus;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@NoArgsConstructor
+@Getter
+@Entity
+@Table(name = "engagements")
+public class Engagement {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @JoinColumn(name = "event_id")
+    @ManyToOne
+    private Schedule schedule;
+
+    @JoinColumn(name = "attendee_id")
+    @ManyToOne
+    private User attendee;
+    private LocalDateTime createdAt = LocalDateTime.now();
+    private RequestStatus requestStatus;
+}
