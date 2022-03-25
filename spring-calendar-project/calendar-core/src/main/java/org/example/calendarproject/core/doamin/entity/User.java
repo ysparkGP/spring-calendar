7c735bf0d457006e8094d3bd75571943d6ffa161
Yusung <qkrdbtjd15@naver.com>
2022-03-25 17:42:26 +0900

    User Entity

---
+++ spring-calendar-project/calendar-core/src/main/java/org/example/calendarproject/core/doamin/entity/User.java
@@ -0,0 +1,36 @@
+package org.example.calendarproject.core.doamin.entity;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Getter
+@NoArgsConstructor
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String email;
+
+    private String password;
+
+    private LocalDate birthday;
+
+    private LocalDateTime createdAt = LocalDateTime.now();
+
+    public User(String name, String email, String password, LocalDate birthday) {
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        this.birthday = birthday;
+    }
+}
