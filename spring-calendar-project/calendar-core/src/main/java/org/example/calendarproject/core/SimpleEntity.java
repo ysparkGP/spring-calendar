f6fea9c2c950c35889cbdc81a191c690f3d35859
Yusung <qkrdbtjd15@naver.com>
2022-03-25 17:46:02 +0900

    ExEntity

---
+++ spring-calendar-project/calendar-core/src/main/java/org/example/calendarproject/core/SimpleEntity.java
@@ -0,0 +1,40 @@
+package org.example.calendarproject.core;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class SimpleEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "SimpleEntity{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
