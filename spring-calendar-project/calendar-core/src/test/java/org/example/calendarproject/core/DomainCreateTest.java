33efafb36860bb2029f83ed405cdc7f816c8b220
Yusung <qkrdbtjd15@naver.com>
2022-03-25 17:45:32 +0900

    Domain Test

---
+++ spring-calendar-project/calendar-core/src/test/java/org/example/calendarproject/core/DomainCreateTest.java
@@ -0,0 +1,25 @@
+package org.example.calendarproject.core;
+
+import org.example.calendarproject.core.doamin.ScheduleType;
+import org.example.calendarproject.core.doamin.entity.Engagement;
+import org.example.calendarproject.core.doamin.Event;
+import org.example.calendarproject.core.doamin.RequestStatus;
+import org.example.calendarproject.core.doamin.entity.Schedule;
+import org.example.calendarproject.core.doamin.entity.User;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class DomainCreateTest {
+
+    @Test
+    void eventCreate(){
+        final User me = new User("me", "email", "pw", LocalDate.now());
+        final Schedule taskSchedule = Schedule.task("할일", "청소하기", LocalDateTime.now(), me);
+        assertEquals(taskSchedule.getScheduleType(), ScheduleType.TASK);
+        assertEquals(taskSchedule.toTask().getTitle(), "할일");
+    }
+}
