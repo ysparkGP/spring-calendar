49382190380944b75a5e90eb7a8abbbdd101756a
Yusung <qkrdbtjd15@naver.com>
2022-03-25 17:42:15 +0900

    Schdule Entity

---
+++ spring-calendar-project/calendar-core/src/main/java/org/example/calendarproject/core/doamin/entity/Schedule.java
@@ -0,0 +1,83 @@
+package org.example.calendarproject.core.doamin.entity;
+
+
+import lombok.*;
+import org.example.calendarproject.core.doamin.Event;
+import org.example.calendarproject.core.doamin.Notification;
+import org.example.calendarproject.core.doamin.ScheduleType;
+import org.example.calendarproject.core.doamin.Task;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "schedules")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder(access = AccessLevel.PRIVATE)
+public class Schedule {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private LocalDateTime startAt;
+
+    private LocalDateTime endAt;
+
+    private String title;
+
+    private String description;
+
+    @JoinColumn(name = "writer_id")
+    @ManyToOne
+    private User writer;
+
+    @Enumerated(EnumType.STRING)
+    private ScheduleType scheduleType;
+
+    @Builder.Default
+    private LocalDateTime createdAt = LocalDateTime.now();
+
+    public static Schedule event(String title, String description, LocalDateTime startAt, LocalDateTime endAt, User writer){
+        return Schedule.builder()
+                .title(title)
+                .description(description)
+                .startAt(startAt)
+                .endAt(endAt)
+                .writer(writer)
+                .scheduleType(ScheduleType.EVENT)
+                .build();
+    }
+
+    public static Schedule task(String title, String description, LocalDateTime taskAt, User writer){
+        return Schedule.builder()
+                .title(title)
+                .description(description)
+                .startAt(taskAt)
+                .writer(writer)
+                .scheduleType(ScheduleType.TASK)
+                .build();
+    }
+
+    public static Schedule task(String title, LocalDateTime notifyAt, User writer){
+        return Schedule.builder()
+                .title(title)
+                .startAt(notifyAt)
+                .writer(writer)
+                .scheduleType(ScheduleType.NOTIFICATION)
+                .build();
+    }
+
+    public Task toTask(){
+        return new Task(this);
+    }
+    public Event toEvent(){
+        return new Event(this);
+    }
+
+    public Notification toNotification(){
+        return new Notification(this);
+    }
+}
