dda6143843d0e669bcfaf6fcaec9fc4b1ad68479
Yusung <qkrdbtjd15@naver.com>
2022-04-01 18:34:49 +0900

    EngagementRepository

---
+++ spring-calendar-project/calendar-core/src/main/java/org/example/calendarproject/core/doamin/entity/repository/EngagementRepository.java
@@ -0,0 +1,7 @@
+package org.example.calendarproject.core.doamin.entity.repository;
+
+import org.example.calendarproject.core.doamin.entity.Engagement;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EngagementRepository extends JpaRepository<Engagement, Long> {
+}
