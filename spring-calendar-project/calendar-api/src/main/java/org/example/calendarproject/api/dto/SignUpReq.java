a18f3f069763b12730ab2cbea0e0eb3646ea74e5
Yusung <qkrdbtjd15@naver.com>
2022-04-01 18:32:28 +0900

    SignUpRequest Dto

---
+++ spring-calendar-project/calendar-api/src/main/java/org/example/calendarproject/api/dto/SignUpReq.java
@@ -0,0 +1,17 @@
+package org.example.calendarproject.api.dto;
+
+import lombok.Data;
+
+import java.time.LocalDate;
+
+@Data
+public class SignUpReq {
+
+    private final String name;
+
+    private final String email;
+
+    private final String password;
+
+    private final LocalDate birthday;
+}
