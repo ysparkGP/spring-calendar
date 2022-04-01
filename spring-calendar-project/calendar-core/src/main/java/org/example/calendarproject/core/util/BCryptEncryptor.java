894ac9d10f196cc0f02dec22b501731e7e158f76
Yusung <qkrdbtjd15@naver.com>
2022-04-01 18:37:48 +0900

    BCryptEncryptor 를 사용한 Encryptor Interface 구현체

---
+++ spring-calendar-project/calendar-core/src/main/java/org/example/calendarproject/core/util/BCryptEncryptor.java
@@ -0,0 +1,22 @@
+package org.example.calendarproject.core.util;
+
+import org.mindrot.jbcrypt.BCrypt;
+import org.springframework.stereotype.Component;
+
+@Component
+public class BCryptEncryptor implements Encryptor{
+    @Override
+    public String encrypt(String origin) {
+        return BCrypt.hashpw(origin, BCrypt.gensalt());
+    }
+
+    @Override
+    public boolean isMatch(String origin, String hashed) {
+        try{
+            return BCrypt.checkpw(origin, hashed);
+        }
+        catch(Exception e){
+            return false;
+        }
+    }
+}
