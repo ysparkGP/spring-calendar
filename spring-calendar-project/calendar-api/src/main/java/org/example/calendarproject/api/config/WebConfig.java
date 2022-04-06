910b1d2e2c4ca8044b6e765620de3fd8bee0c9ca
Yusung <qkrdbtjd15@naver.com>
2022-04-06 20:04:08 +0900

    dev 가 아닐 때, WebConfig

---
+++ spring-calendar-project/calendar-api/src/main/java/org/example/calendarproject/api/config/WebConfig.java
@@ -0,0 +1,18 @@
+package org.example.calendarproject.api.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
+
+@Profile("!dev")
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers){
+        resolvers.add(new AuthUserResolver());
+    }
+}
