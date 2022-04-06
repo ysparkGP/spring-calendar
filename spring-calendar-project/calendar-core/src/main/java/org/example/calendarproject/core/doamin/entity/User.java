a6e346a1e40632cbe87fe1603958363bf25de4a4
Yusung <qkrdbtjd15@naver.com>
2022-04-06 20:01:44 +0900

    Builder 패턴 적용

--- spring-calendar-project/calendar-core/src/main/java/org/example/calendarproject/core/doamin/entity/User.java
+++ spring-calendar-project/calendar-core/src/main/java/org/example/calendarproject/core/doamin/entity/User.java
@@ -1,28 +1,23 @@
 package org.example.calendarproject.core.doamin.entity;
 
-import lombok.Getter;
+import lombok.Builder;
 import lombok.NoArgsConstructor;
 import org.example.calendarproject.core.util.Encryptor;
 
-import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.Table;
 import java.time.LocalDate;
-import java.time.LocalDateTime;
 
-@Getter
 @NoArgsConstructor
 @Entity
 @Table(name = "users")
 public class User extends BaseEntity{
-
-
     private String name;
-
     private String email;
-
-    private String password;
-
+    private String password; // hashed
     private LocalDate birthday;
 
+    @Builder
     public User(String name, String email, String password, LocalDate birthday) {
         this.name = name;
         this.email = email;
@@ -30,8 +25,20 @@ public class User extends BaseEntity{
         this.birthday = birthday;
     }
 
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public LocalDate getBirthday() {
+        return birthday;
+    }
     // strategy 패턴... 인터페이스를 인자로 넘겨서 기능 위임을 하게된다.
-    public boolean isMatch(Encryptor encryptor, String password) {
+    public boolean isMatched(Encryptor encryptor, String password) {
         return encryptor.isMatch(password, this.password);
     }
+
 }
