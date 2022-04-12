134f543ac810a4e5c8ca978e60af9df591c6baa6
Yusung <qkrdbtjd15@naver.com>
2022-04-12 17:00:03 +0900

    EventCreate Dto(+validation)

--- spring-calendar-project/calendar-api/src/main/java/org/example/calendarproject/api/dto/EventCreateReq.java
+++ spring-calendar-project/calendar-api/src/main/java/org/example/calendarproject/api/dto/EventCreateReq.java
@@ -5,6 +5,8 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import java.time.LocalDateTime;
 import java.util.List;
 
@@ -13,9 +15,12 @@ import java.util.List;
 @Getter
 @Setter
 public class EventCreateReq {
+    @NotBlank
     private String title;
     private String description;
+    @NotNull
     private LocalDateTime startAt;
+    @NotNull
     private LocalDateTime endAt;
     private List<Long> attendeeIds;
 }
