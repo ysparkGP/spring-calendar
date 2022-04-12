93f55894f6ebb0053e5ac22087dd5b251be6f46a
Yusung <qkrdbtjd15@naver.com>
2022-04-12 16:53:10 +0900

    Engagement Repository

--- spring-calendar-project/calendar-core/src/main/java/org/example/calendarproject/core/doamin/entity/repository/EngagementRepository.java
+++ spring-calendar-project/calendar-core/src/main/java/org/example/calendarproject/core/doamin/entity/repository/EngagementRepository.java
@@ -3,5 +3,8 @@ package org.example.calendarproject.core.doamin.entity.repository;
 import org.example.calendarproject.core.doamin.entity.Engagement;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface EngagementRepository extends JpaRepository<Engagement, Long> {
+    List<Engagement> findAllByAttendee_Id(Long userId);
 }
