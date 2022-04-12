b2f611d5be128a9e14a743908a09de2278dd6c1e
Yusung <qkrdbtjd15@naver.com>
2022-04-12 16:58:49 +0900

    Schedule Dto

---
+++ spring-calendar-project/calendar-api/src/main/java/org/example/calendarproject/api/dto/ScheduleDto.java
@@ -0,0 +1,8 @@
+package org.example.calendarproject.api.dto;
+
+import org.example.calendarproject.core.doamin.ScheduleType;
+
+public interface ScheduleDto {
+
+    ScheduleType getScheduleType();
+}
