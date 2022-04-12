57e16d0c6cd2700e812f94308d102cd1ecb6bf83
Yusung <qkrdbtjd15@naver.com>
2022-04-12 16:51:25 +0900

    기존 스케줄과 겹치는지 확인해주는 중간 객체 Period

---
+++ spring-calendar-project/calendar-core/src/main/java/org/example/calendarproject/core/util/Period.java
@@ -0,0 +1,46 @@
+package org.example.calendarproject.core.util;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+
+public class Period {
+    private final LocalDateTime startAt;
+    private final LocalDateTime endAt;
+
+    private Period(LocalDateTime startAt, LocalDateTime endAt) {
+        this.startAt = startAt;
+        if(endAt == null){
+            this.endAt = startAt;
+        }
+        else this.endAt = endAt;
+    }
+
+    public static Period of(LocalDateTime startAt, LocalDateTime endAt){
+        return new Period(startAt, endAt);
+    }
+    public static Period of(LocalDate startDate, LocalDate endDate){
+
+        return new Period(startDate.atStartOfDay(), LocalDateTime.of(endDate, LocalTime.of(23,59,59,9999999)));
+    }
+
+    public boolean isOverlapped(LocalDateTime startAt, LocalDateTime endAt) {
+        return this.startAt.isBefore(endAt) && startAt.isBefore(this.endAt);
+    }
+
+    public boolean isOverlapped(LocalDate date) {
+        return isOverlapped(date.atStartOfDay(), LocalDateTime.of(date, LocalTime.of(23,59,59,9999999)));
+    }
+
+    public LocalDateTime getStartAt() {
+        return startAt;
+    }
+
+    public LocalDateTime getEndAt() {
+        return endAt;
+    }
+
+    public boolean isOverlapped(Period period) {
+        return isOverlapped(period.startAt, period.endAt);
+    }
+}
