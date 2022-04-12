a958d18abff2a550cd069efa1ca49850e6747a5f
Yusung <qkrdbtjd15@naver.com>
2022-04-12 16:53:03 +0900

    Schedule Repository

--- spring-calendar-project/calendar-core/src/main/java/org/example/calendarproject/core/doamin/entity/repository/ScheduleRepository.java
+++ spring-calendar-project/calendar-core/src/main/java/org/example/calendarproject/core/doamin/entity/repository/ScheduleRepository.java
@@ -3,5 +3,9 @@ package org.example.calendarproject.core.doamin.entity.repository;
 import org.example.calendarproject.core.doamin.entity.Schedule;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ScheduleRepository extends JpaRepository<Schedule,Long> {
+
+    public List<Schedule> findAllByWriter_Id(Long userId);
 }
