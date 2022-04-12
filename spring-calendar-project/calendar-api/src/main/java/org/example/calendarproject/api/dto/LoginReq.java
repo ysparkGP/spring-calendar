2dd0dc3073ba7a87810b8e876907220c933e280a
Yusung <qkrdbtjd15@naver.com>
2022-04-12 16:59:37 +0900

    Login Dto(+validation)

--- spring-calendar-project/calendar-api/src/main/java/org/example/calendarproject/api/dto/LoginReq.java
+++ spring-calendar-project/calendar-api/src/main/java/org/example/calendarproject/api/dto/LoginReq.java
@@ -2,6 +2,8 @@ package org.example.calendarproject.api.dto;
 
 import lombok.*;
 
+import javax.validation.constraints.Size;
+
 @Getter
 @Setter
 @AllArgsConstructor
@@ -9,6 +11,6 @@ import lombok.*;
 public class LoginReq {
 
     private String email;
-
+    @Size(min = 6, message = "6자리 이상 입력해주세요.")
     private String password;
 }
