6939cb22c72ad1a79e9790abc77d951dd34c4d6d
Yusung <qkrdbtjd15@naver.com>
2022-04-12 16:58:33 +0900

    SignUp Dto(+validation)

--- spring-calendar-project/calendar-api/src/main/java/org/example/calendarproject/api/dto/SignUpReq.java
+++ spring-calendar-project/calendar-api/src/main/java/org/example/calendarproject/api/dto/SignUpReq.java
@@ -2,6 +2,10 @@ package org.example.calendarproject.api.dto;
 
 import lombok.*;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.time.LocalDate;
 
 @Getter
@@ -10,11 +14,17 @@ import java.time.LocalDate;
 @NoArgsConstructor
 public class SignUpReq {
 
+    @NotBlank
     private String name;
 
+    @Email
+    @NotBlank
     private String email;
 
+    @Size(min = 6, message = "6자리 이상 입력해주세요.")
+    @NotBlank
     private String password;
 
+    @NotNull
     private LocalDate birthday;
 }
