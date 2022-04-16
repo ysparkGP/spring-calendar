b5ec2651733345ca036267d0f2613bd00d968c9d
Yusung <qkrdbtjd15@naver.com>
2022-04-16 16:31:47 +0900

    FakeEmailService(dev -> test)

--- spring-calendar-project/calendar-api/src/main/java/org/example/calendarproject/api/service/FakeEmailService.java
+++ spring-calendar-project/calendar-api/src/main/java/org/example/calendarproject/api/service/FakeEmailService.java
@@ -1,15 +1,14 @@
 package org.example.calendarproject.api.service;
 
-import org.example.calendarproject.core.doamin.entity.Engagement;
+import org.example.calendarproject.api.dto.EngagementEmailStuff;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
-@Profile("dev")
+@Profile("test")
 @Service
 public class FakeEmailService implements EmailService{
     @Override
-    public void sendEngagement(Engagement engagement) {
-        System.out.println("send email. email : " + engagement.getAttendee().getEmail() +
-                ", scheduleId : " + engagement.getSchedule().getId());
+    public void sendEngagement(EngagementEmailStuff stuff) {
+        System.out.println("send email. email : " + stuff.getSubject());
     }
 }
