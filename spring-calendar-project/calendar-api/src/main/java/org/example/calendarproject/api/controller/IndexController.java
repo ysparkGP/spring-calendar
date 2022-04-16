53c145900696e7963fdd699b6c7f42e54137ef29
Yusung <qkrdbtjd15@naver.com>
2022-04-16 16:37:04 +0900

    Index Controller(+updateEngagement)

--- spring-calendar-project/calendar-api/src/main/java/org/example/calendarproject/api/controller/IndexController.java
+++ spring-calendar-project/calendar-api/src/main/java/org/example/calendarproject/api/controller/IndexController.java
@@ -1,8 +1,10 @@
 package org.example.calendarproject.api.controller;
 
+import org.example.calendarproject.core.doamin.RequestReplyType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpSession;
@@ -21,4 +23,16 @@ public class IndexController {
         return "index";
     }
 
+    @GetMapping("/events/engagements/{engagementId}")
+    public String updateEngagement(@PathVariable Long engagementId,
+                                   @RequestParam RequestReplyType type,
+                                   Model model,
+                                   HttpSession httpSession)
+    {
+        model.addAttribute("isSignIn", httpSession.getAttribute(LOGIN_SESSION_KEY) != null);
+        model.addAttribute("updateType", type.name());
+        model.addAttribute("engagementId",engagementId);
+        model.addAttribute("path","/events/engagements/" + engagementId + "?type=" + type.name());
+        return "update-event";
+    }
 }
