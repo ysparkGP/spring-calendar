ea2c6bde046fb8e3f8acaa03703ee3b704fecf9c
Yusung <qkrdbtjd15@naver.com>
2022-04-16 16:28:28 +0900

    Event Dto

--- spring-calendar-project/calendar-core/src/main/java/org/example/calendarproject/core/doamin/Event.java
+++ spring-calendar-project/calendar-core/src/main/java/org/example/calendarproject/core/doamin/Event.java
@@ -3,6 +3,7 @@ package org.example.calendarproject.core.doamin;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.example.calendarproject.core.doamin.entity.Schedule;
+import org.example.calendarproject.core.util.Period;
 
 import java.time.LocalDateTime;
 
@@ -18,5 +19,13 @@ public class Event {
 
     public boolean isOverlapped(LocalDateTime startAt, LocalDateTime endAt) {
         return schedule.getStartAt().isBefore(endAt) && startAt.isBefore(schedule.getEndAt());
+    }
+
+    public String getTitle(){
+        return this.schedule.getTitle();
+    }
+
+    public Period getPeriod() {
+        return Period.of(this.schedule.getStartAt(), this.schedule.getEndAt());
     }
 }
