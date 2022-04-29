351a9e972f3d826dc1f34c5075dc9390dc83d0ac
Yusung <qkrdbtjd15@naver.com>
2022-04-29 20:32:21 +0900

    Email Serivce Interface (+sendAlarmMail, sendShareRequestMail)

--- spring-calendar-project/calendar-api/src/main/java/org/example/calendarproject/api/service/EmailService.java
+++ spring-calendar-project/calendar-api/src/main/java/org/example/calendarproject/api/service/EmailService.java
@@ -1,7 +1,13 @@
 package org.example.calendarproject.api.service;
 
+import org.example.calendarproject.api.controller.BatchController;
 import org.example.calendarproject.api.dto.EngagementEmailStuff;
+import org.example.calendarproject.core.doamin.entity.Share;
 
 public interface EmailService {
     void sendEngagement(EngagementEmailStuff stuff);
+
+    void sendAlarmMail(BatchController.SendMailBatchReq r);
+
+    void sendShareRequestMail(String email, String name, Share.Direction direction);
 }
