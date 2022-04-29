60cc20bee26c398c31f4a3813b49684d5169fe4e
Yusung <qkrdbtjd15@naver.com>
2022-04-29 20:31:53 +0900

    Fake Email Service

--- spring-calendar-project/calendar-api/src/main/java/org/example/calendarproject/api/service/FakeEmailService.java
+++ spring-calendar-project/calendar-api/src/main/java/org/example/calendarproject/api/service/FakeEmailService.java
@@ -1,6 +1,8 @@
 package org.example.calendarproject.api.service;
 
+import org.example.calendarproject.api.controller.BatchController;
 import org.example.calendarproject.api.dto.EngagementEmailStuff;
+import org.example.calendarproject.core.doamin.entity.Share;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
@@ -10,5 +12,15 @@ public class FakeEmailService implements EmailService{
     @Override
     public void sendEngagement(EngagementEmailStuff stuff) {
         System.out.println("send email. email : " + stuff.getSubject());
+    }
+
+    @Override
+    public void sendAlarmMail(BatchController.SendMailBatchReq r) {
+        System.out.println("send alarm" + r.toString());
+    }
+
+    @Override
+    public void sendShareRequestMail(String email, String name, Share.Direction direction) {
+        System.out.println("send share request mail. " + email + ", " + name +", " + direction);
     }
 }
