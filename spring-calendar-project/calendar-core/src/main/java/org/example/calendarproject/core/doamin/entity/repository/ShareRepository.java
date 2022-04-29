d4a017d8576b703b97c4dfdb836def13d6839245
Yusung <qkrdbtjd15@naver.com>
2022-04-29 20:25:58 +0900

    ShareRepository

---
+++ spring-calendar-project/calendar-core/src/main/java/org/example/calendarproject/core/doamin/entity/repository/ShareRepository.java
@@ -0,0 +1,7 @@
+package org.example.calendarproject.core.doamin.entity.repository;
+
+import org.example.calendarproject.core.doamin.entity.Share;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ShareRepository extends JpaRepository<Share, Long> {
+}
