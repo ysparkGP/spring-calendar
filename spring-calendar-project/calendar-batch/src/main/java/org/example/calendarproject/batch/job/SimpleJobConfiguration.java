65fbad928ffc7cf0baee9f284eab5b230e2c1425
Yusung <qkrdbtjd15@naver.com>
2022-04-29 20:27:08 +0900

    testJob

---
+++ spring-calendar-project/calendar-batch/src/main/java/org/example/calendarproject/batch/job/SimpleJobConfiguration.java
@@ -0,0 +1,38 @@
+package org.example.calendarproject.batch.job;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.JobScope;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Slf4j
+@RequiredArgsConstructor
+@Configuration
+public class SimpleJobConfiguration {
+    private final JobBuilderFactory jobBuilderFactory; // 생성자 DI 받음
+    private final StepBuilderFactory stepBuilderFactory; // 생성자 DI 받음
+
+    @Bean
+    public Job simpleJob() {
+        return jobBuilderFactory.get("simpleJob")
+                .start(simpleStep())
+                .build();
+    }
+
+    @Bean
+    @JobScope
+    public Step simpleStep() {
+        return stepBuilderFactory.get("simpleStep")
+                .tasklet((contribution, chunkContext) -> {
+                    log.info(">>>>> This is Step1");
+                    return RepeatStatus.FINISHED;
+                })
+                .build();
+    }
+}
