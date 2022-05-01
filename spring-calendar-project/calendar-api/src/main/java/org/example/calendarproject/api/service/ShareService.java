39fc152bc77c0374e96aa8915ff0fb6dce762179
Yusung <qkrdbtjd15@naver.com>
2022-05-01 18:50:23 +0900

    Share Service(+ findSharedUserIdsByUser)

--- spring-calendar-project/calendar-api/src/main/java/org/example/calendarproject/api/service/ShareService.java
+++ spring-calendar-project/calendar-api/src/main/java/org/example/calendarproject/api/service/ShareService.java
@@ -14,6 +14,10 @@ import org.example.calendarproject.core.service.UserService;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 @Service
 @RequiredArgsConstructor
 public class ShareService {
@@ -41,5 +45,30 @@ public class ShareService {
                 .filter(share -> share.getRequestStatus() == RequestStatus.REQUESTED)
                 .map(share -> share.reply(type))
                 .orElseThrow(()-> new CalendarException(ErrorCode.BAD_REQUEST));
+    }
+
+    /**
+     * 공유 대상 조회
+     *
+     * 자신과 양방향 공유관계인 상대방(자신이 TO, From 둘 다 가능)
+     * 내가 공유관계의 수신자(toUser) 인 경우 & 단 방향
+     * @param authUser
+     * @return
+     */
+
+    @Transactional
+    public List<Long> findSharedUserIdsByUser(AuthUser authUser) {
+        final Stream<Long> biDirectionShares = shareRepository.findAllByDirection(
+                authUser.getId(),
+                RequestStatus.ACCEPTED,
+                Share.Direction.BI_DIRECTION
+        ).stream().map(s -> s.getToUserId().equals(authUser.getId())? s.getFromUserId() : s.getToUserId());
+        final Stream<Long> uniDirectionShares = shareRepository.findAllByToUserIdAndRequestStatusAndDirection(
+                authUser.getId(),
+                RequestStatus.ACCEPTED,
+                Share.Direction.UNI_DIRECTION
+        ).stream().map(s -> s.getFromUserId());
+
+        return Stream.concat(biDirectionShares, uniDirectionShares).collect(Collectors.toList());
     }
 }
