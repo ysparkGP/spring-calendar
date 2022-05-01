557b3a281121fe19bbff6db571d7bfc96b14a2cb
Yusung <qkrdbtjd15@naver.com>
2022-05-01 18:49:43 +0900

    Share Repository(+ jpql)

--- spring-calendar-project/calendar-core/src/main/java/org/example/calendarproject/core/doamin/entity/repository/ShareRepository.java
+++ spring-calendar-project/calendar-core/src/main/java/org/example/calendarproject/core/doamin/entity/repository/ShareRepository.java
@@ -1,7 +1,16 @@
 package org.example.calendarproject.core.doamin.entity.repository;
 
+import org.example.calendarproject.core.doamin.RequestStatus;
 import org.example.calendarproject.core.doamin.entity.Share;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface ShareRepository extends JpaRepository<Share, Long> {
+
+    @Query("SELECT s from Share s where (s.fromUserId = ?1 or s.toUserId = ?1) and s.requestStatus = ?2 and s.direction = ?3")
+    List<Share> findAllByDirection(Long userId, RequestStatus requestStatus, Share.Direction biDirection);
+
+    List<Share> findAllByToUserIdAndRequestStatusAndDirection(Long userId, RequestStatus requestStatus, Share.Direction biDirection);
 }
